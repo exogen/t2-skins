216abaa3ca8ed01f93add9939cd8d87aed36b122
Brian Beck <exogen@gmail.com>
2024-10-26 18:06:15 -0700

    Add newSkins to JSON manifest

--- build.mjs
+++ build.mjs
@@ -2,6 +2,12 @@ import fs from "fs";
 import { globby } from "globby";
 import orderBy from "lodash.orderby";
 
+const ONE_DAY = 1000 * 60 * 60 * 24;
+const THIRTY_DAYS = ONE_DAY * 30;
+const NINE_MONTHS = THIRTY_DAYS * 9;
+
+const nineMonthsAgo = Date.now() - NINE_MONTHS;
+
 const models = [
   "lmale",
   "mmale",
@@ -61,6 +67,50 @@ async function getSkinManifest() {
             const match = name.match(/\/([^/]+)\/weapon_\w+\.png$/);
             return match[1];
           }),
+          [(name) => name.toLowerCase()],
+          ["asc"]
+        );
+        return skins;
+      }, {}),
+    },
+    newSkins: {
+      ...models.reduce((skins, name, i) => {
+        skins[name] = orderBy(
+          customSkins[i]
+            .map((filename) => {
+              const dateModified = fs.statSync(filename).mtime;
+              return {
+                filename,
+                dateModified,
+              };
+            })
+            .filter((details) => details.dateModified.getTime() > nineMonthsAgo)
+            .map((details) =>
+              details.filename.replace(
+                /(^.*\/|\.[lmh](male|female|bioderm)\.png$)/g,
+                ""
+              )
+            ),
+          [(name) => name.toLowerCase()],
+          ["asc"]
+        );
+        return skins;
+      }, {}),
+      ...weaponModels.reduce((skins, name, i) => {
+        skins[name] = orderBy(
+          customWeaponSkins[i]
+            .map((filename) => {
+              const dateModified = fs.statSync(filename).mtime;
+              return {
+                filename,
+                dateModified,
+              };
+            })
+            .filter((details) => details.dateModified.getTime() > nineMonthsAgo)
+            .map(
+              (details) =>
+                details.filename.match(/\/([^/]+)\/weapon_\w+\.png$/)[1]
+            ),
           [(name) => name.toLowerCase()],
           ["asc"]
         );
